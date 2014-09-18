8ffa33d03fc32d76c97eac044a6094ba6033d045
dantedubon <dante.dubon@gmail.com>
2014-09-18 16:06:53 -0500

    deleting object

---
+++ ConsoleApplicationTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcklenAvenue.Email;
+
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = AcklenSmtpClient.From("dante.dubon@gmail.com");
+            client.Send("dante.dubon@gmail.com","texto plano","texto plano");
+
+        }
+    }
+}
